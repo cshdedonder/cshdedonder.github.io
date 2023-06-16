d3f5da5854ddf5c10889e19025be417236d6b376
Cedric De Donder <cedric.dedonder@gmail.com>
2023-06-16 11:36:19 +0200

    Removed key hardcoding

--- commute/script.js
+++ commute/script.js
@@ -136,3 +136,7 @@ function initMaps() {
         }
     });
 }
+
+function getURLParameter(name) {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+}
